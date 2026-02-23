c8475d735392ead44a57d0bc0850f9aa8c24f8a0
FlareFlo <mail@flareflo.dev>
2026-02-23 12:14:02 +0100

    Add display

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,53 @@
 #![no_std]
 #![no_main]
 
+use chrono::NaiveTime;
+use embedded_graphics::text::Baseline;
+use embedded_graphics::prelude::{DrawTarget, Point};
+use embedded_graphics::text::Text;
+use embedded_graphics::pixelcolor::BinaryColor;
+use embedded_graphics::mono_font::ascii::{FONT_10X20, FONT_6X10, FONT_7X13};
+use embedded_graphics::mono_font::MonoTextStyleBuilder;
+use ssd1306::{I2CDisplayInterface, Ssd1306};
 use cortex_m::prelude::_embedded_hal_serial_Read;
+use embedded_graphics::Drawable;
 use defmt::*;
+use ssd1306::prelude::*;
 use defmt_rtt as _;
 use panic_probe as _;
 
 use embedded_io_async::Read;
 use embassy_executor::Spawner;
-use embassy_rp::bind_interrupts;
-use embassy_rp::peripherals::UART0;
-// NOTICE: We imported BufferedInterruptHandler here instead of InterruptHandler
-use embassy_rp::uart::{BufferedUartRx, Config, BufferedInterruptHandler};
-use heapless::String;
+use embassy_rp::{bind_interrupts, i2c};
+use embassy_rp::i2c::{Async, I2c};
+use embassy_rp::peripherals::{I2C1, UART0};
+use embassy_rp::uart::{BufferedUartRx, BufferedInterruptHandler};
+use embassy_rp::uart;
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_sync::channel::Channel;
+use embassy_time::Timer;
+use heapless::{format, String};
 use nmea::Nmea;
+use core::fmt::Write;
+use embassy_sync::signal::Signal;
 
-// Bind the UART0 interrupt to Embassy's internal buffered handler
 bind_interrupts!(struct Irqs {
     UART0_IRQ => BufferedInterruptHandler<UART0>;
+    I2C1_IRQ => i2c::InterruptHandler<I2C1>;
 });
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
 
-    // Set to 38400 baud as per the seller's specs
-    let mut config = Config::default();
+    let t = do_display(I2c::new_async(p.I2C1, p.PIN_27, p.PIN_26, Irqs, i2c::Config::default()));
+    spawner.spawn(t).unwrap();
+
+    let mut config = uart::Config::default();
     config.baudrate = 115200;
 
-    // Create a backing array for the interrupt ring buffer
     let mut rx_buf = [0u8; 512];
 
-    // Initialize BufferedUartRx
     let mut uart_rx = BufferedUartRx::new(p.UART0, Irqs, p.PIN_1, &mut rx_buf, config);
 
     let mut nmea = Nmea::default();
@@ -47,16 +63,24 @@ async fn main(_spawner: Spawner) {
                 let b = byte[0] as char;
 
                 if b == '\n' {
-                    if let Ok(_) = nmea.parse(buffer.as_str()) {
-                        info!("Fix: {} Sats: {}", nmea.fix_type, nmea.satellites().len());
+                    if let Ok(mtype) = nmea.parse(buffer.as_str()) {
+                        let mut state = DisplayState::default();
+                        let sats = nmea.satellites().len();
+                        state.sats = sats as _;
+                        info!("{} Fix: {} Sats: {}", mtype, nmea.fix_type, sats);
 
                         // Checking only for latitude and longitude
                         if let (Some(lat), Some(lon)) = (nmea.latitude, nmea.longitude) {
                             info!("Lat: {} | Lon: {}", lat, lon);
-                        } else {
-                            debug!("Tracking... waiting for fix.");
+                            state.lat = lat;
+                            state.lon = lon;
                         }
 
+                        if let Some(time) = nmea.fix_timestamp() {
+                            state.time = time;
+                        }
+
+                        DISPLAY_SIGNAL.signal(state);
                     }
                     buffer.clear();
                 } else if b != '\r' {
@@ -67,5 +91,55 @@ async fn main(_spawner: Spawner) {
                 error!("UART Read Error: {:?}", e);
             }
         }
+    }
+}
+
+#[derive(Clone, Default)]
+pub struct DisplayState {
+    time: NaiveTime,
+    lat: f64,
+    lon: f64,
+    sats: u8,
+}
+
+static DISPLAY_SIGNAL: Signal<CriticalSectionRawMutex, DisplayState> = Signal::new();
+#[embassy_executor::task]
+async fn do_display(i2c: I2c<'static, I2C1, Async>) {
+    let interface = I2CDisplayInterface::new(i2c);
+    let mut display = Ssd1306::new(
+        interface,
+        DisplaySize128x32,
+        DisplayRotation::Rotate0,
+    ).into_buffered_graphics_mode();
+    display.init().unwrap();
+
+    let text_style = MonoTextStyleBuilder::new()
+        .font(&FONT_6X10)
+        .text_color(BinaryColor::On)
+        .build();
+
+    let mut state = DisplayState::default();
+    loop {
+        display.clear(BinaryColor::Off).unwrap();
+
+        if DISPLAY_SIGNAL.signaled() {
+            let d = DISPLAY_SIGNAL.wait().await;
+            state = d;
+        };
+
+        Text::with_baseline(&format!(15; "{} {}", state.time, state.sats).unwrap(), Point::zero(), text_style, Baseline::Top)
+            .draw(&mut display)
+            .unwrap();
+
+        Text::with_baseline(&format!(10; "N{:.5}", state.lat).unwrap(), Point::new(0, 9), text_style, Baseline::Top)
+            .draw(&mut display)
+            .unwrap();
+
+        Text::with_baseline(&format!(10; "E{:.5}", state.lon).unwrap(), Point::new(0, 18), text_style, Baseline::Top)
+            .draw(&mut display)
+            .unwrap();
+
+        display.flush().unwrap();
+        Timer::after_millis(40).await;
     }
 }
